f7bca38931f1d99396cc657f2b60f6176fcc0557
absence18 <baek0518@naver.com>
2021-06-08 12:12:37 +0900

    간단한 추석 추가

--- app/src/main/java/com/example/myqr/MainActivity.java
+++ app/src/main/java/com/example/myqr/MainActivity.java
@@ -16,15 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new IntentIntegrator(this).initiateScan();
+        // Scanner 객체 생성 .. App실행 -> Scanner 실행
+        // MainActivity 내부에 카메라Scanner 생성
     }
-    @Override
+    @Override // MainActivity랑 CameraScanner랑 통신하는 method.. 스캔되면 메소드 실행.
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null) {
-            if(result.getContents() == null) {
+            if(result.getContents() == null) { // 스캔, 데이터 없으면 Cancelled 출력 됨
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
                 // todo
-            } else {
+            } else { // 스캔 성공하면 qr데이타 출력
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 // todo
             }
